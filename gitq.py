4ae3aabab2f6b9c82aa809579d39bad38ed3dc55
Luke Murphy <lukewm@riseup.net>
2015-11-17 19:43:18 +0100

    add basic formatting for quote
    python gitq.py quote "Donald Knuth" - works a charm right now.

--- gitq.py
+++ gitq.py
@@ -15,11 +15,18 @@ import sys
 import docopt
 import random
 import wikiquote
+import textwrap
 
 
 if __name__ == "__main__":
     args = docopt.docopt(__doc__, argv=sys.argv[1:])
     if args['quote']:
         name = args['<name>']
-        quote = random.choice(wikiquote.quotes(name))
-        print(name, quote, sep=" -> ")
+
+        quote_selection = wikiquote.quotes(name, max_quotes=100)
+        single_quote = random.choice(quote_selection)
+
+        wrapped_quote = '\n'.join(textwrap.wrap(single_quote, 80))
+        wrapped_indented_quote = textwrap.indent(wrapped_quote, '   ')
+
+        print(name, wrapped_indented_quote, sep='\n')
