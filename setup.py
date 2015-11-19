da7eb30ac8d34a996e768e149913e09813c7a913
Luke Murphy <lukewm@riseup.net>
2015-11-19 22:42:49 +0100

    fix wrong quotes, add classifiers

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name="gitq",
-    version="0.0.1",
-    author="Luke Murphy",
-    author_email="lukewm@riseup.net",
-    description="@TODO",
-    license="@TODO",
-    keywords="git",
-    url="@TODO",
+    name='gitq',
+    version='0.0.1',
+    author='Luke Murphy',
+    author_email='lukewm@riseup.net',
+    description='@TODO',
+    license='@TODO',
+    keywords='git',
+    url='@TODO',
     packages=find_packages(exclude='tests'),
     long_description=read('README.md'),
     install_requires=[
@@ -22,5 +22,16 @@ setup(
         'wheel',
         'wikiquote'
     ],
-    classifiers=['@TODO'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Version Control',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
