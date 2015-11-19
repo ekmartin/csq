6c81ae3173fd1023faffc2c27a13ce379e018b54
Luke Murphy <lukewm@riseup.net>
2015-11-19 23:25:11 +0100

    add stubs for next tests, fix comments, remove test

--- tests/test_gitq.py
+++ tests/test_gitq.py
@@ -6,9 +6,12 @@ import subprocess
 
 class TestGitqInternals:
     def test_humans_quote(self):
-        """Should be able to retrieve a quote from all humans in HUMANS"""
-        for human in gitq.HUMANS:
-            random.choice(wikiquote.quotes(human))
+        """Should be retrieve a quote from all humans in gitq.HUMANS"""
+        pass
+        # @TODO - this is a slow test, let's comment it out for now
+        #         could be a candidate for a `--fast` option
+        # for human in gitq.HUMANS:
+        #     random.choice(wikiquote.quotes(human))
 
 
 class TestGitqCLI:
@@ -16,38 +19,54 @@ class TestGitqCLI:
         return s.replace('\n', '')
 
     def test_docopt_string(self):
-        """Should return docstring when gitq is called with no arguments"""
+        """Should return docstring when calling `gitq` without arguments"""
         args = ['gitq']
         r1 = subprocess.check_output(args).decode()
         assert self.strip_stdout(gitq.__doc__) == self.strip_stdout(r1)
 
     def test_docopt_string_with_short_help_arg(self):
-        """Should return docstring when gitq is called with short help arg"""
+        """Should return docstring when passing `-h` argument"""
         args = ['gitq', '-h']
         r1 = subprocess.check_output(args).decode()
         assert self.strip_stdout(gitq.__doc__) == self.strip_stdout(r1)
 
     def test_docopt_string_with_long_help_arg(self):
-        """Should return docstring when gitq is called with long help arg"""
+        """Should return docstring when passing `--help` argument"""
         args = ['gitq', '--help']
         r1 = subprocess.check_output(args).decode()
         assert self.strip_stdout(gitq.__doc__) == self.strip_stdout(r1)
 
     def test_long_version(self):
-        """Should return version when gitq is called with long version arg"""
+        """Should return version when passing `--version` argument"""
         args = ['gitq', '--version']
         r1 = subprocess.check_output(args).decode()
         assert self.strip_stdout(r1) == gitq.VERSION
 
     def test_short_version(self):
-        """Should return version when gitq is called with short version arg"""
+        """Should return version when passing `quote` argument"""
         args = ['gitq', '-v']
         r1 = subprocess.check_output(args).decode()
         assert self.strip_stdout(r1) == gitq.VERSION
 
     def test_quote(self):
-        """Should return quote when gitq is called with quote argument"""
+        """Should return quote when passing `quote` argument"""
         args = ['gitq', 'quote']
         r1 = subprocess.check_output(args).decode()
         assert isinstance(r1, str)
         assert (delim in r1 for delim in ['|>>', '<<|'])
+
+    def test_short_enable_fails(self):
+        """Should return an error when no .git/ directory present and passing `-e` argument"""
+        pass
+
+    def test_short_enable_succeeeds(self):
+        """Should add correct code to .git/post-commit and passing `-e` argument"""
+        pass
+
+    def test_long_enable_fails(self):
+        """Should return an error when no .git/ directory present and passing `--enable` argument"""
+        pass
+
+    def test_long_enable_succeeeds(self):
+        """Should add correct code to .git/post-commit when passing `--enable` argument"""
+        pass
